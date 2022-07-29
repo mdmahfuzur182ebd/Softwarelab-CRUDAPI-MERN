beda6fe22917fd842adc96441e3d6bc9a126582d
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-07-29 18:15:32 +0600

    CRUD Done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,45 +1,64 @@
+const { v4: uuidv4 } = require("uuid");
 
-const getAllUsers = (req , res) => {
-   res.status(200).json({
-      message: "All users",
-    });
+const User = require("../models/user.model");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-const getOneUsers = (req , res) => {
-   res.status(200).json({
-      message: "Get One users",
-    });
+const getOneUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-
-const createUsers = (req , res) => {
-     
-   res.status(201).json({
-      message: "User is Created",
+const createUser = async (req, res) => {
+  try {
+    const newUser = new User({
+      id: uuidv4(),
+      name: req.body.name,
+      age: Number(req.body.age),
     });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-
-
-
-const updateUsers = (req , res) => {
-   res.status(200).json({
-      message: "Update Users",
-    });
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    user.name = req.body.name;
+    user.age = Number(req.body.age);
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-
-const deleteUsers = (req , res) => {
-   res.status(200).json({
-      message: "Delete users",
-    });
+const deleteUser = async (req, res) => {
+  try {
+    await User.deleteOne({ id: req.params.id });
+    res.status(200).json({ message: "user is deleted" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-
-module.exports ={
-    getAllUsers, 
-    getOneUsers, 
-    createUsers, 
-    updateUsers, 
-    deleteUsers,
-}
+module.exports = {
+  getAllUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
